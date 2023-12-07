2909c9f2938be9c0b12a19a37e546e9e92be8d82
munenepeter <peternjega7@gmail.com>
2023-12-07 09:08:59 +0300

    print all files given a path

--- main.c
+++ main.c
@@ -1,16 +1,29 @@
 #include<stdio.h>
+#include<dirent.h>
 
-void print_sth(char *str){
- printf("%s\n",str);
+void print_files(const char *dir){
+ //open the directory
+ DIR *dir = opendir(dir);
+
+ if(dir == NULL){
+  fprintf(stderr, "Error: Could not open %s because of %s", dir, errno);
+ exit(1);
 }
+ struct dirent* entry;
+ 
+ while((entry = readdir(dir)) != NULL){
+  printf("%s\n", entry->d_name;
+} 
+
+ closedir(dir);
+}
+
 
 int main(){
 
- char *name = "Peter Munene";
- print_sth(name);
+ char *dir = ".";
 
- printf("%p\n",name);
+ print_files(dir);
 
- printf("Hello World\n");
  return  0;
 }
