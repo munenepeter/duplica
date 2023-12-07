b6b75606d70bac7177384990e4076fd7de127276
munenepeter <peternjega7@gmail.com>
2023-12-07 08:44:55 +0300

    revise functions & pointers

--- main.c
+++ main.c
@@ -1,6 +1,15 @@
 #include<stdio.h>
 
+void print_sth(char *str){
+ printf("%s\n",str);
+}
+
 int main(){
+
+ char *name = "Peter Munene";
+ print_sth(name);
+
+ printf("%p\n",name);
 
  printf("Hello World\n");
  return  0;
